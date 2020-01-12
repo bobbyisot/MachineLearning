522f2a832d457ea6d14f8f8c72fca44bc15f59db
YB Wan <995985317@qq.com>
2020-01-12 14:24:41 +0800

    first commit

---
+++ myML/myml.py
@@ -0,0 +1,7 @@
+class Test_2020(object):
+    def __init__(self):
+        self.a = 1
+        print(f'success')
+
+    def add_a(self):
+        self.a += 1
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name = "test-wyb",
+    version = "0.0.1",
+    keywords =("data-mining"),
+    description = "test",
+    author = "WYB",
+    author_email = "995985317@qq.com",
+    license = "MIT License",
+    packages = find_packages()
+)
